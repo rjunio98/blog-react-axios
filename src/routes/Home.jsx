d88f0fe66475986aa5c8776d9aa4594780cee27f
rjunio98 <robledorjunior1@gmail.com>
2023-08-01 17:23:12 -0300

    resgatando dados da api

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
+import "./Home.css"
 
 const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async() => {
+    try {
+      const response = await axios.get("https;//jsonplaceholder.typicode.com/posts")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+
+    getPosts()
+  }, [])
+
+
   return (
     <div>Home</div>
   )
