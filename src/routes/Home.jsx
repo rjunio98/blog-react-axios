6d79e99e6025fcf73c68a8f0759916977ac8460d
rjunio98 <robledorjunior1@gmail.com>
2023-08-01 18:24:37 -0300

    exibindo posts

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,6 +10,9 @@ const Home = () => {
   const getPosts = async() => {
     try {
       const response = await axios.get("https;//jsonplaceholder.typicode.com/posts")
+      const data = response.data
+
+      setPosts(data)
     } catch (error) {
       console.log(error)
     }
@@ -22,7 +25,18 @@ const Home = () => {
 
 
   return (
-    <div>Home</div>
+    <div>
+      <h1>Últimos posts</h1>
+      {posts.lenght === 0 ? (<p>Carregando...</p>) : (
+        posts.map((post) => {
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        })
+      )}
+    </div>
   )
 }
 
